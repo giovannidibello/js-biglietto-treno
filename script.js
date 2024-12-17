06744a4dd90d043735e5164c8c3711a18952cd39
Giovanni <dibellogio@gmail.com>
2024-12-17 15:30:44 +0100

    add logic human code

--- script.js
+++ script.js
@@ -3,3 +3,22 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 // - L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// INPUT
+
+// chiede all'utente il numero di chilometri che vuole percorrere
+// chiede all'utente l'età del passeggero
+
+// ELABORAZIONE
+
+// definizione prezzo base del biglietto
+// SE l'utente è minorenne 
+    // applica uno sconto del 20%
+// SE l'utente è over 65
+    // applica uno sconto del 40%
+// ALTRIMENTI
+    // applica il prezzo normale
+
+// OUTPUT
+
+// restituisci il prezzo normale con massimo due decimali
